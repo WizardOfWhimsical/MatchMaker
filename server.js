817dd7ba7e793627e49811f9a4c4b01a35b5f82f
Stephen Lewis <st.rayis1085@gmail.com>
2025-04-26 11:40:43 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import morgan from "morgan"
+import { resolve } from "path"
+import { writeFile, readFile } from "fs/promises"
+import config from "#config";
+// import compareUsers from "#compare"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(express.static("./public"));
+
+app.listen(config.port, () => {
+    console.log(`Listening @ ${config.port}`);
+  });
+
+  let friendsPath = resolve() +"/data/friends.json";
+let friends;
+// console.log(friendsPath)
+
+
+readFile(friendsPath, "utf8")
+.then( (r) => {
+friends = JSON.parse(r)
+})
+
+app.get("/count", (req, res)=>{
+  res.type("text/plain")
+  res.send(`${friends.length}`)
+})
+
+app.post("/compareProfile", (req, res)=>{
+  let me = req.body;
+  let bestMatch = compareUsers(me, friends);
+  res.json(bestMatch);
+})
+
+app.post("/addProfile", (req, res)=>{
+let addOn= friends.length +1;
+let person = req.body;
+person.name = person.name+"_"+addOn;
+friends.push(person)
+friends = JSON.stringify(friends, null, 4)
+writeFile(friendsPath, friends)
+res.json(person)
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("404: Page Not Found, Sorry")
+})
+
