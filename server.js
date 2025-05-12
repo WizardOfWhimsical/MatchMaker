2006c34b5c854747f3be771f828a4aacbf40ec71
Stephen Lewis <st.rayis1085@gmail.com>
2025-05-11 23:18:07 -0400

    trying to fix server connection

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
-app.use(express.static("/public"));
+app.use(express.static("./public"));
 
 app.listen(config.port, () => {
     console.log(`Listening @ ${config.port}`);
